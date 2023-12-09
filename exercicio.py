99dbb22127e18371bbe8a8f2f8cc8c7926b9e36f
Pedro Corado <pedrocorado2025@gmail.com>
2023-12-09 17:08:58 -0300

    Python: Fluxo Condicional & Repetição

---
+++ exercicio.py
@@ -0,0 +1,71 @@
+propaganda_online = [
+    {'tempo_gasto_site': 68.95, 'idade': 35, 'renda_area': 61833.90,
+    'tempo_gasto_internet': 256.09, 'cidade': 'Wrightburgh',
+    'pais': 'Tunisia', 'clicou_no_ad': 0
+    },
+    {'tempo_gasto_site': 80.23, 'idade': 31, 'renda_area': 68441.85,
+    'tempo_gasto_internet': 193.77, 'cidade': 'West Jodi',
+    'pais': 'Nauru', 'clicou_no_ad': 0
+    },
+    {'tempo_gasto_site': 69.47, 'idade': 26, 'renda_area': 59785.94,
+    'tempo_gasto_internet': 236.50, 'cidade': 'Davidton',
+    'pais': 'San Marino', 'clicou_no_ad': 0
+    },
+    {'tempo_gasto_site': 68.37, 'idade': 35, 'renda_area': 73889.99,
+    'tempo_gasto_internet': 225.58, 'cidade': 'South Manuel',
+    'pais': 'Iceland', 'clicou_no_ad': 0
+    },
+    {'tempo_gasto_site': 88.91, 'idade': 33, 'renda_area': 53852.85,
+    'tempo_gasto_internet': 208.36, 'cidade': 'Brandonstad',
+    'pais': 'Myanmar', 'clicou_no_ad': 0
+    },
+    {'tempo_gasto_site': None, 'idade': 48, 'renda_area': 24593.33,
+    'tempo_gasto_internet': 131.76, 'cidade': 'Port Jefferybury',
+    'pais': 'Australia', 'clicou_no_ad': 1
+    },
+    {'tempo_gasto_site': 74.53, 'idade': 30, 'renda_area': 68862.00,
+    'tempo_gasto_internet': 221.51, 'cidade': 'West Colin',
+    'pais': 'Grenada'},
+    {'tempo_gasto_site': 69.88, 'idade': 20, 'renda_area': 55642.32,
+    'tempo_gasto_internet': 183.82, 'cidade': 'Ramirezton',
+    'pais': 'Ghana', 'clicou_no_ad': 0
+    }
+]
+
+# Aqui eu fiz a interação de cada idade utilizando o for.
+# E utilizei o if para saber se a idade e igual ou maior que 30 anos
+
+
+paises = []
+
+for dado_de_usuario in propaganda_online:
+    if dado_de_usuario['idade'] >= 30:
+        paises.append(dado_de_usuario['pais'])
+print(paises)
+
+
+#Estou fazendo a verificação para saber quem clicou no add.
+#Quando a pessoa clica em um anuncio , fica com o valor de 1 , e quando nao clica , fica com valor 0
+#Na estrutura de comparação eu "pegunto" se tiver clicou_no_ad in dado_de_usuario
+#E dado_de_usuario["valor de" clicou_no_ad] nao for vasio e dado_de_usuario[valor] for >= 1
+#Ele vai adicionar a renda dos usuarios no array leads.
+
+
+leads = []
+
+for dado_de_usuario in propaganda_online:
+    if 'clicou_no_ad' in dado_de_usuario and dado_de_usuario['clicou_no_ad'] is not None and dado_de_usuario['clicou_no_ad'] >= 1:
+        leads.append(dado_de_usuario['renda_area'])
+
+print(leads)
+
+
+#Aqui eu fiz basicamente a MESMA coisa que o exercicio de cima 
+
+
+cidades = []
+
+for dado_de_usuario in propaganda_online:
+    if 'tempo_gasto_site' in dado_de_usuario and dado_de_usuario['tempo_gasto_site'] is not None and dado_de_usuario['tempo_gasto_site'] >= 70.00:
+        cidades.append(dado_de_usuario['cidade'])
+print(cidades)
